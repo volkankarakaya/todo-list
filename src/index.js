4216488d5ba55ca74136add2964ec582653e59df
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-03-20 13:24:12 +0300

    add project list item click event listener

--- src/index.js
+++ src/index.js
@@ -6,8 +6,16 @@ import "../node_modules/@fortawesome/fontawesome-free/js/all.js";
 const todoImg = document.getElementById("todoLogo");
 todoImg.src = todoLogo;
 
-let activeCategory = 'home';
-const homeBtn = document.getElementById('home')
+let activeCategory = "home";
+const homeBtn = document.getElementById("home");
+const projectsBtn = document.querySelector(".projects h3");
+homeBtn.addEventListener("click", function () {
+  activeCategory = "home";
+  updateTodoList(todoList.home)
+});
+projectsBtn.addEventListener("click", function () {
+  activeCategory = "projects";
+});
 
 function TodoItem(title, descripton, dueDate, priority) {
   this.title = title;
@@ -28,15 +36,15 @@ document
   });
 
 const addTodoBtn = document.getElementById("add-todo-btn");
-const todoList = {}
-todoList.home = JSON.parse(localStorage.getItem("home")|| '[]');
+const todoList = {};
+todoList.home = JSON.parse(localStorage.getItem("home") || "[]");
 
 addTodoBtn.addEventListener("click", function (e) {
-    const titleValue = document.getElementById("title").value;
-    const dateValue = document.getElementById("dueDate").value;
-    const priorityValue = document.getElementById("priority").value;
-    const descriptonValue = document.getElementById("description").value;
-    
+  const titleValue = document.getElementById("title").value;
+  const dateValue = document.getElementById("dueDate").value;
+  const priorityValue = document.getElementById("priority").value;
+  const descriptonValue = document.getElementById("description").value;
+
   //create object
   e.preventDefault();
   let todoItem = new TodoItem(
@@ -47,31 +55,31 @@ addTodoBtn.addEventListener("click", function (e) {
   );
   // save item to LocalStorage
   todoList.home.push(todoItem);
-  localStorage.setItem("home", JSON.stringify(todoList.home))
-  
+  localStorage.setItem("home", JSON.stringify(todoList.home));
+
   // console.log(todoList.home);
   //reset form values
-  resetForm()
+  resetForm();
 
-  updateTodoList(todoList.home)
+  updateTodoList(todoList.home);
 });
 
-function resetForm(){
-    const titleValue = document.getElementById("title").value =""
-    const dateValue = document.getElementById("dueDate").value ="";
-    const priorityValue = document.getElementById("priority").value ="";
-    const descriptonValue = document.getElementById("description").value ="";
+function resetForm() {
+  const titleValue = (document.getElementById("title").value = "");
+  const dateValue = (document.getElementById("dueDate").value = "");
+  const priorityValue = (document.getElementById("priority").value = "");
+  const descriptonValue = (document.getElementById("description").value = "");
 }
 
-function updateTodoList(todoList){
-    const itemContainer = document.querySelector('.item-container');
-    itemContainer.innerHTML = ""
+function updateTodoList(list) {
+  const itemContainer = document.querySelector(".item-container");
+  itemContainer.innerHTML = "";
 
-    todoList.forEach(element => {
-        const htmlItem = document.createElement('div');
-        // htmlItem.setAttribute('class',);
-        htmlItem.setAttribute('class', 'item '+ element.priority)
-        htmlItem.innerHTML = `<div class="item-name">
+  list.forEach((element) => {
+    const htmlItem = document.createElement("div");
+    // htmlItem.setAttribute('class',);
+    htmlItem.setAttribute("class", "item " + element.priority);
+    htmlItem.innerHTML = `<div class="item-name">
           <input type="checkbox" name="todo" id="" />
           <p>${element.title}</p>
         </div>
@@ -81,66 +89,71 @@ function updateTodoList(todoList){
           <i class="fa fa-regular fa-pen-to-square"></i>
           <i class="fa fa-solid fa-trash-can"></i>
         </div>
-      </div>`
+      </div>`;
 
-
-        
-
-    itemContainer.appendChild(htmlItem)        
-    });
-
+    if (element.category === activeCategory) {
+      itemContainer.appendChild(htmlItem);
+    }
+  });
 }
 
-
 const addProjectBtn = document.querySelector(".add-project-btn");
-addProjectBtn.addEventListener("click", function(){
-    document.querySelector(".save-project").classList.toggle("active")
-})
+addProjectBtn.addEventListener("click", function () {
+  document.querySelector(".save-project").classList.toggle("active");
+});
 
 window.addEventListener("load", updateTodoList(todoList.home));
 
+todoList.projects = JSON.parse(localStorage.getItem("projects") || "{}");
+const saveProjectBtn = document.querySelector(".save-project button");
 
+saveProjectBtn.addEventListener("click", function () {
+  const projectInputValue = document.querySelector(".save-project input").value;
+  const saveProjectContainer = document.querySelector(".save-project");
+  saveProjectContainer.classList.remove("active");
 
+  todoList.projects[projectInputValue] = [];
+  localStorage.setItem("projects", JSON.stringify(todoList.projects) || {}) ||
+    {};
 
+  console.log(todoList.projects);
+  updateProjectList(todoList.projects);
+  updateTodoList(todoList.home);
+});
 
-todoList.projects = JSON.parse(localStorage.getItem("projects")||"{}")
-const saveProjectBtn = document.querySelector(".save-project button")
-
-
-saveProjectBtn.addEventListener("click", function(){
-    const projectInputValue = document.querySelector(".save-project input").value;
-    const saveProjectContainer = document.querySelector(".save-project");
-    saveProjectContainer.classList.remove("active");
-
-
-    todoList.projects[projectInputValue] = [] 
-    localStorage.setItem("projects",JSON.stringify(todoList.projects)||{}) || {};
-
-    console.log(todoList.projects)
-    updateProjectList(todoList.projects)
-    
-})
-
-
-function updateProjectList(projects){
-  const projectList = document.querySelector('.projects ul');
-  projectList.innerHTML = ""
-  Object.keys(projects).forEach(key=>{
-    const newItem = document.createElement('li');
-    newItem.textContent = key
-    projectList.appendChild(newItem)
-  })
-
-
+function updateProjectList(projects) {
+  const projectList = document.querySelector(".projects ul");
+  projectList.innerHTML = "";
+  Object.keys(projects).forEach((key) => {
+    const newItem = document.createElement("li");
+    newItem.textContent = key;
+    newItem.setAttribute("data-category", key);
+    projectList.appendChild(newItem);
+  });
 }
 
+// //project list click event
+// const projectListItems = document.querySelectorAll("li")
+// // console.log('projectListItems')
+// projectListItems.forEach(project => {
+//   project.addEventListener("click", function (e) {
+//     // const projectList = todoList.projects[project];
+//     activeCategory = e.target.dataset.category;
+//     console.log(e.target.dataset.category);
+//     updateTodoList(todoList.home);
+//     console.log('todoList')
+//   });
+// });
+
+
 window.addEventListener("load", updateProjectList(todoList.projects));
+window.addEventListener("load", updateTodoList(todoList.home));
 
-const projectListItems = document.querySelectorAll(".projects li");
-projectListItems.forEach(project=>{
-  project.addEventListener("click", function(){
-    const projectList = todoList.projects[project];
-    updateTodoList(projectList);
-
+const projectListItems = document.querySelectorAll("li");
+projectListItems.forEach(function(listItem){
+  listItem.addEventListener("click", function(e){
+    activeCategory = e.target.dataset.category;
+    updateTodoList(todoList.home)
   })
 })
+console.log(projectListItems)
